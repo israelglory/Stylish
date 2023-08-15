1d71809fe1af735c691d82ae0f27eecba108a4eb
israelglory <iglory6@gmail.com>
2023-08-16 00:23:29 +0100

    done with bottom tab

--- src/stacks/tabStack.js
+++ src/stacks/tabStack.js
@@ -5,6 +5,8 @@ import
 from 'react-native-vector-icons/MaterialCommunityIcons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { primaryColor } from '../constants/colors';
+import {View, Platform} from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 import HomeStack from './homeStack';
 import WishlistScreen from '../screens/wishlist';
@@ -25,50 +27,68 @@ function TabStack() {
           headerTitleStyle: { fontWeight: 'bold' },
           tabBarStyle: {
            paddingTop: 10,
-           paddingBottom: 10,
-           height: 60,
+           paddingBottom: 20,
+           height: 76,
           },
           tabBarActiveTintColor: primaryColor,
           tabBarInactiveTintColor: 'gray',
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-            if (route.name === 'HomeStack') {
-              iconName = focused
-                ? 'web'
-                : 'web';
-            } else if (route.name === 'WishlistStack') {
-              iconName = focused
-                ? 'account-settings'
-                : 'account-settings-outline';
-            }else if (route.name === 'CartStack') {
-              iconName = focused
-                ? 'heart'
-                : 'heart-outline';
-            }else if (route.name === 'SearchStack') {
-              iconName = focused
-                ? 'account-settings'
-                : 'account-settings-outline';
-            }else if (route.name === 'AccountStack') {
-              iconName = focused
-                ? 'account-settings'
-                : 'account-settings-outline';
-            }
-            return (
-              <MaterialCommunityIcons
-                name={iconName}
-                size={30}
-                color={color}
-              />
-            );
-          }
+          tabBarLabelStyle: {
+              fontSize: 13,
+              fontFamily: 'Montserrat-Medium',
+          },
+          // tabBarIcon: ({ focused, color, size }) => {
+          //   let iconName;
+          //   if (route.name === 'HomeStack') {
+          //     iconName = focused
+          //       ? 'web'
+          //       : 'web';
+          //   } else if (route.name === 'WishlistStack') {
+          //     iconName = focused
+          //       ? 'account-settings'
+          //       : 'account-settings-outline';
+          //   }else if (route.name === 'CartStack') {
+
+          //     iconName = focused
+          //       ? 'heart'
+          //       : 'heart-outline';
+          //   }else if (route.name === 'SearchStack') {
+          //     iconName = focused
+          //       ? 'account-settings'
+          //       : 'account-settings-outline';
+          //   }else if (route.name === 'AccountStack') {
+          //     iconName = focused
+          //       ? 'account-settings'
+          //       : 'account-settings-outline';
+          //   }
+          //   return (
+          //     <MaterialCommunityIcons
+          //       name={iconName}
+          //       size={30}
+          //       color={color}
+          //     />
+          //   );
+          // }
         })}>
         <Tab.Screen
           name="HomeStack"
           component={HomeStack}
           options={{
             tabBarLabel: 'Home',
-
+           
             headerShown: false,
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+                iconName = focused
+                  ? 'home'
+                  : 'home';
+              return (
+                <Icon
+                  name={iconName}
+                  size={30}
+                  color={color}
+                />
+              );
+            }
 
           }}  />
         <Tab.Screen
@@ -76,29 +96,93 @@ function TabStack() {
           component={WishlistScreen}
           options={{
             tabBarLabel: 'Wishlist',
-            headerShown: false
+            headerShown: false,
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+                iconName = focused
+                  ? 'hearto'
+                  : 'hearto';
+              return (
+                <Icon
+                  name={iconName}
+                  size={30}
+                  color={color}
+                />
+              );
+            }
           }} />
           <Tab.Screen
           name="CartStack"
           component={CartScreen}
           options={{
-            tabBarLabel: 'Cart',
-            headerShown: false
+            tabBarLabel: '',
+            headerShown: false,
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+                iconName = focused
+                  ? 'heart'
+                  : 'heart-outline';
+              return (
+                <View
+                style={{
+                  top: Platform.OS === 'ios' ? -10 : -20,
+                  width: Platform.OS === 'ios' ? 50 : 60,
+                  height: Platform.OS === 'ios' ? 50 : 60,
+                  borderRadius: Platform.OS === 'ios' ? 25 : 30,
+                  backgroundColor: focused ? primaryColor : '#fff',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  shadowColor: '#000',
+                  elevation: 5,
+                }}>
+                <Icon
+                  name="shoppingcart"
+                  size={Platform.OS === 'ios' ? 24 : 30}
+                  color={focused ? '#ffff' : '#000'}
+                />
+              </View>
+              );
+            }
           }} />
           <Tab.Screen
           name="SearchStack"
           component={SearchScreen}
           options={{
             tabBarLabel: 'Search',
-            headerShown: false
+            headerShown: false,
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+                iconName = focused
+                  ? 'search1'
+                  : 'search1';
+              return (
+                <Icon
+                  name={iconName}
+                  size={30}
+                  color={color}
+                />
+              );
+            }
           }} />
           <Tab.Screen
           name="AccountStack"
           component={SettingsScreen}
           options={{
             tabBarLabel: 'Settings',
-            title: 'Settings',
-            headerShown: false
+            headerShown: false,
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+                iconName = focused
+                  ? 'setting'
+                  : 'setting';
+              return (
+                <Icon
+                  name={iconName}
+                  size={30}
+                  color={color}
+                />
+              );
+            }
           }} />
       </Tab.Navigator>
   );
